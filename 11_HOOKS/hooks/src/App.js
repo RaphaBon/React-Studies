a76eabd1bfeee92cb788e86f8589c468c9bdddd0
RaphaBack <raphaelalvaresbonjardim@gmail.com>
2026-08-10 16:38:43 -0300

    feat: study useContext hook

--- 11_HOOKS/hooks/src/App.js
+++ 11_HOOKS/hooks/src/App.js
@@ -7,27 +7,32 @@ import { Route, BrowserRouter, Routes, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 
+//Aula 9 -> useContext()
+import {HookUseContext} from './components/HookUseContext';
+
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
 
-      <BrowserRouter>
-        {/* "Fake NavBar" */}
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about" >About</Link>
-          </li>
-        </ul>
+      <HookUseContext>
+        <BrowserRouter>
+          {/* "Fake NavBar" */}
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about" >About</Link>
+            </li>
+          </ul>
 
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />}/>
-        </Routes>
-      </BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />}/>
+          </Routes>
+        </BrowserRouter>
+      </HookUseContext>
 
     </div>
   );
