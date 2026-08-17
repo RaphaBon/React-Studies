3134c8aa06ed4515002c0b8ee6494f2b84d90589
RaphaBack <raphaelalvaresbonjardim@gmail.com>
2026-08-17 16:09:18 -0300

    feat: server config with Express and middlewares (cors, json, urlencoded)

---
+++ 12_REACTGRAM/backend/app.js
@@ -0,0 +1,25 @@
+//Variables
+require("dotenv").config()
+const port = process.env.PORT
+
+//Imports
+const express = require("express")
+const cors = require("cors")
+const path = require("path")
+
+const { router } = require("./routes/Router")
+
+const app = express()
+
+// Middlewares
+app.use(cors({credentials: true, origin: "htpp://localhost:3000"}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
+// Routes
+app.use(router)
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`)
+})
